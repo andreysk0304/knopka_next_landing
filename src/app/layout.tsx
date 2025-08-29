366bf24cea11a064ee88fed0b2c0cc301f9515d1
Kravchenko Andrew <andrey.kravchenko@megaplan.ru>
2025-08-29 14:06:56 +0300

    remove unused libs, add metric key

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { Inter } from 'next/font/google'
 import { ConfigProvider } from 'antd'
 import ruRU from 'antd/locale/ru_RU'
 import './globals.css'
-import Head from 'next/head'
 import { YandexMetric } from '@/components/YandexMetric'
 import { THEME } from './theme'
 import { ClientWrapper } from '../components/ClientWrapper'
@@ -13,14 +12,14 @@ const inter = Inter({ subsets: ['latin', 'cyrillic'] })
 export const metadata: Metadata = {
 	title: 'Кнопка Заказы - Автоматизация бизнеса на маркетплейсах',
 	description: 'Отслеживайте продажи, анализируйте тренды и принимайте обоснованные решения',
+	other: {
+		'yandex-verification': 'fafcf84179d9ab7c',
+	},
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ru">
-			<Head>
-				<meta name="yandex-verification" content="fafcf84179d9ab7c" />
-			</Head>
 			<body className={inter.className}>
 				<YandexMetric />
 				<ConfigProvider theme={THEME} locale={ruRU}>
